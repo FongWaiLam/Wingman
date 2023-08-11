bd9c40de1644388607e5df09d9436e287d2e20df
Fong Wai Lam <2694026l@student.gla.ac.uk>
2023-08-11 01:38:57 +0100

    Update RFIDRepositoryTest.java

--- src/test/java/com/fwl/unmannedstore/respository/RFIDRepositoryTest.java
+++ src/test/java/com/fwl/unmannedstore/respository/RFIDRepositoryTest.java
@@ -21,32 +21,32 @@ class RFIDRepositoryTest {
     @Autowired
     private StoreRepository storeRepository;
 
-    @Test
-    public void printRFIDFullList() {
+    // @Test
+    // public void printRFIDFullList() {
 
-        System.out.println(rfidRepository.findAll());
-    }
+    //     System.out.println(rfidRepository.findAll());
+    // }
 
-    @Test
-    public void printRFIDByProduct() {
-        Product firstProduct = productRepository.findById(1).get();
-        List<RFID> rfidList = rfidRepository.findByProduct(firstProduct);
-        System.out.println(rfidList);
-    }
+    // @Test
+    // public void printRFIDByProduct() {
+    //     Product firstProduct = productRepository.findById(1).get();
+    //     List<RFID> rfidList = rfidRepository.findByProduct(firstProduct);
+    //     System.out.println(rfidList);
+    // }
 
-    @Test
-    public void addRFIDtoExistingProduct(){
-        Store glasgowStore = storeRepository.findById(1).get();
-        Product abBloodTypeCup = productRepository.findById(2).get();
-        RFID abBloodTypeCupRFID2 = RFID.builder()
-                .epc("e28011606000020ea907f4af").product(abBloodTypeCup).store(glasgowStore).build();
-        rfidRepository.save(abBloodTypeCupRFID2);
-    }
+    // @Test
+    // public void addRFIDtoExistingProduct(){
+    //     Store glasgowStore = storeRepository.findById(1).get();
+    //     Product abBloodTypeCup = productRepository.findById(2).get();
+    //     RFID abBloodTypeCupRFID2 = RFID.builder()
+    //             .epc("e28011606000020ea907f4af").product(abBloodTypeCup).store(glasgowStore).build();
+    //     rfidRepository.save(abBloodTypeCupRFID2);
+    // }
 
-    @Test
-    public void printRFIDBasedOnProductAndStore() {
-        Store glasgowStore = storeRepository.findById(1).get();
-        Product abBloodTypeCup = productRepository.findById(2).get();
-        System.out.println(rfidRepository.findByProductAndStore(abBloodTypeCup, glasgowStore));
-    }
+    // @Test
+    // public void printRFIDBasedOnProductAndStore() {
+    //     Store glasgowStore = storeRepository.findById(1).get();
+    //     Product abBloodTypeCup = productRepository.findById(2).get();
+    //     System.out.println(rfidRepository.findByProductAndStore(abBloodTypeCup, glasgowStore));
+    // }
 }
